3aa177f14e4537547aa5540912af6e60480f30aa
alexandre.tang <alexandre.tang@polytechnique.edu>
2022-12-10 14:39:34 +0100

    ajout de l'authentification

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-3_mAZBxRo7q', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1c37e80n95a68l20qkttfnr4v9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west_3' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'alexandretang.com/test' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
